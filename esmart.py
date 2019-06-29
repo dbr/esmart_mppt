ac4d5446d8e32858e78b151869afa782b5b8b791
dbr <dbr.onix@gmail.com>
2019-06-29 21:35:52 +0930

    Initial rework to write to InfluxDB

--- esmart.py
+++ esmart.py
@@ -1,6 +1,8 @@
 # Library for communicating with eSmart 3 MPPT charger
 # skagmo.com, 2018
 
+from typing import List, Optional, Callable
+
 import struct, time, serial, socket, requests
 #from collections import namedtuple
 
@@ -14,82 +16,101 @@ LOAD_ON = b"\xaa\x01\x01\x02\x04\x04\x01\x00\xfd\x13\x39"
 
 DEVICE_MODE = ["IDLE", "CC", "", "FLOAT", "STARTING"]
 
+
+class SolarData(object):
+    def __init__(self, chg_mode: int, pv_volt: float, bat_volt: float,
+                 chg_cur: float, load_volt: float, load_cur: float, chg_power: int,
+                 load_power: int, bat_temp: int, int_temp: int, soc: int,
+                 co2_gram: int):
+        self.chg_mode = chg_mode
+        self.pv_volt = pv_volt
+        self.bat_volt = bat_volt
+        self.chg_cur = chg_cur
+        self.load_volt = load_volt
+        self.load_cur = load_cur
+        self.chg_power = chg_power
+        self.load_power = load_power
+        self.bat_temp = bat_temp
+        self.int_temp = int_temp
+        self.soc = soc
+        self.co2_gram = co2_gram
+
+
 class Esmart:
     def __init__(self) -> None:
         self.state = STATE_START
-        self.data = []
-        self.callback = False
+        self.data: List[int] = []
+        self.callback: Optional[Callable] = None
         self.port = ""
         self.timeout = 0
+        self.ser: Optional[serial.Serial] = None
 
-    def __del__(self):
+    def __del__(self) -> None:
         self.close()
 
-    def set_callback(self, function):
+    def set_callback(self, function: Callable) -> None:
         self.callback = function
 
     def open(self, port):
-        self.ser = serial.Serial(port,9600,timeout=0.1)
+        self.ser = serial.Serial(port, 9600, timeout=0.1)
         self.port = port
 
     def close(self):
-        try:
+        if self.ser is not None:
             self.ser.close()
-            self.ser = False
-        except AttributeError:
-            pass
+            self.ser = None
 
     def send(self, pl):
         self.ser.write(self.pack(pl))
 
-    def parse(self, data):
+    def parse(self, data) -> None:
         for c in data:
-            if (self.state == STATE_START):
-                if (c == 0xaa):
+            if self.state == STATE_START:
+                if c == 0xaa:
                     # Start character detected
                     self.state = STATE_DATA
                     self.data = []
                     self.target_len = 255
-                #else:
-                    #print c
 
-            elif (self.state == STATE_DATA):
+            elif self.state == STATE_DATA:
                 self.data.append(c)
                 # Received enough of the packet to determine length
-                if (len(self.data) == 5):
+                if len(self.data) == 5:
                     self.target_len = 6 + self.data[4]
 
                 # Received whole packet
-                if (len(self.data) == self.target_len):
+                if len(self.data) == self.target_len:
                     self.state = STATE_START
 
                     #print " ".join("{:02x}".format(ord(c)) for c in self.data)
 
                     # Source 3 is MPPT device
-                    if (self.data[2] == 3):
+                    if self.data[2] == 3:
                         msg_type = self.data[3]
 
                         # Type 0 packet contains most data
-                        if (self.data[3] == 0):
-                            fields = {}
-                            fields['chg_mode'] = int.from_bytes(self.data[7:9], byteorder='little')
-                            fields['pv_volt'] = int.from_bytes(self.data[9:11], byteorder='little') / 10.0
-                            fields['bat_volt'] = int.from_bytes(self.data[11:13], byteorder='little') / 10.0
-                            fields['chg_cur'] = int.from_bytes(self.data[13:15], byteorder='little') / 10.0
-                            fields['load_volt'] = int.from_bytes(self.data[17:19], byteorder='little') / 10.0
-                            fields['load_cur'] = int.from_bytes(self.data[19:21], byteorder='little') / 10.0
-                            fields['chg_power'] = int.from_bytes(self.data[21:23], byteorder='little')
-                            fields['load_power'] = int.from_bytes(self.data[23:25], byteorder='little')
-                            fields['bat_temp'] = self.data[25]
-                            fields['int_temp'] = self.data[27]
-                            fields['soc'] = self.data[29]
-                            fields['co2_gram'] = int.from_bytes(self.data[33:35], byteorder='little')
+                        if self.data[3] == 0:
+                            data = SolarData(
+                                chg_mode=int.from_bytes(self.data[7:9], byteorder='little'),
+                                pv_volt=int.from_bytes(self.data[9:11], byteorder='little') / 10.0,
+                                bat_volt=int.from_bytes(self.data[11:13], byteorder='little') / 10.0,
+                                chg_cur=int.from_bytes(self.data[13:15], byteorder='little') / 10.0,
+                                load_volt=int.from_bytes(self.data[17:19], byteorder='little') / 10.0,
+                                load_cur=int.from_bytes(self.data[19:21], byteorder='little') / 10.0,
+                                chg_power=int.from_bytes(self.data[21:23], byteorder='little'),
+                                load_power=int.from_bytes(self.data[23:25], byteorder='little'),
+                                bat_temp=self.data[25],
+                                int_temp=self.data[27],
+                                soc=self.data[29],
+                                co2_gram=int.from_bytes(self.data[33:35], byteorder='little'),
+                            )
 
-                            self.callback(fields)
+                            if self.callback is not None:
+                                self.callback(data)
 
     def tick(self):
         try:
-            while (self.ser.inWaiting()):
+            while self.ser.inWaiting():
                 self.parse(self.ser.read(100))
 
             # Send poll packet to request data every 5 seconds
@@ -105,7 +126,7 @@ class Esmart:
             opened = 0
             while not opened:
                 try:
-                    self.ser = serial.Serial(self.port,38400,timeout=0)
+                    self.ser = serial.Serial(self.port, 38400, timeout=0)
                     time.sleep(0.5)
                     if self.ser.read(100):
                         opened = 1
@@ -115,4 +136,3 @@ class Esmart:
                     time.sleep(0.5)
                     self.ser.close()
             print("Error fixed")
-
